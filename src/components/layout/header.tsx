d93c6e5982cd57a36d9501d8b1a4cd38bfaa79b1
What Is up <isupw1441@gmail.com>
2025-07-30 17:02:53 +0000

    remove the guarantee page entirely now

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,7 +15,6 @@ import { Separator } from '../ui/separator';
 const navLinks = [
   { href: '/services', label: 'Services' },
   { href: '/process', label: 'Process' },
-  { href: '/guarantee', label: 'Guarantee' },
   { href: '/results', label: 'Case Studies' },
   { href: '/blog', label: 'Blog' },
   { href: '/community', label: 'Community' },
